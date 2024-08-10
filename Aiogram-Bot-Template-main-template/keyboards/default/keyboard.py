a6ca619d2f455ba259e5dcb4f0c4741afb81ddf4
bekzodidiye <148998731+bekzodidiye@users.noreply.github.com>
2024-08-10 21:20:09 +0500

    Delete Aiogram-Bot-Template-main-template directory

--- Aiogram-Bot-Template-main-template/keyboards/default/keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
-from aiogram import types
-
-
-
-nik=ReplyKeyboardMarkup(resize_keyboard=True,)
-nik_btn=KeyboardButton(text='𝓝𝓘𝓚 𝓨𝓐𝓡𝓐𝓣𝓘𝓢𝓗')
-nik.add(nik_btn)
